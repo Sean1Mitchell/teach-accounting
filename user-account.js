525c894a3df17656e309fcb2f212d7850cbaf392
Sean Mitchell <seanmitchell09022000@gmail.com>
2026-05-16 00:32:18 +0100

    update

---
+++ user-account.js
@@ -0,0 +1,5 @@
+function toggleSidebar() {
+    document.querySelector('.sidebar').classList.toggle('active')
+    document.querySelector('header').classList.toggle('active')
+    document.querySelector('.main-content').classList.toggle('active');
+}
